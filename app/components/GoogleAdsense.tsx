a3825918a59b01274ded84cb598fddd41b4ac4a8
UFOBob069 <UFOBob069@yahoo.com>
2025-02-22 20:19:12 -0600

    Fix AdSense initialization using Next.js Script component

--- app/components/GoogleAdsense.tsx
+++ app/components/GoogleAdsense.tsx
@@ -1,23 +1,15 @@
 "use client";
 
-import { useEffect } from 'react';
+import Script from 'next/script';
 
 export default function GoogleAdsense() {
-  useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (err) {
-      console.error('AdSense error:', err);
-    }
-  }, []);
-
   return (
-    <>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0131222257759084"
-        crossOrigin="anonymous"
-      />
-    </>
+    <Script
+      id="adsbygoogle-init"
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0131222257759084"
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+    />
   );
 } 
